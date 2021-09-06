815d36b9c71a26e6b50ee41fae62c84f2afaf9a9
Arijit Roy (azmuth47) <15376408+arijit192@users.noreply.github.com>
2021-09-06 22:21:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,12 +8,13 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 mkdir ~/an
 cd ~/an
-#repo init --depth=1 -u git://codeaurora.org/platform/manifest.git -b release -m LA.UM.9.6.2.r1-04800-89xx.0.xml
-repo init --depth=1 -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0
+repo init --depth=1 -u git://codeaurora.org/platform/manifest.git -b release -m LA.UM.9.6.2.r1-04800-89xx.0.xml
+#repo init --depth=1 -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0
 repo sync
 source build/envsetup.sh
 #export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C"
 #choosecombo 1 aosp_arm64 eng
-lunch arrow_lavender-userdebug
-make vendorimage
+#lunch arrow_lavender-userdebug
+lunch
+mka vendorimage
 #m bacon
